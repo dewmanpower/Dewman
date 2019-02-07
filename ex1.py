3f529ec899d2b1239f09d5b8623aac7644c9226c
dewmanpower <40836069+dewmanpower@users.noreply.github.com>
2019-02-07 21:59:28 +0700

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,7 @@
+
+"https://www.practicepython.org/exercise/2014/01/29/01-character-input.html"
+name = input("Input your name:")
+ 
+age = input("Input your age:")
+age = int(age)
+print("Your name is " + name +" and "+str(100-age)+" years later you will reach age of 100")
