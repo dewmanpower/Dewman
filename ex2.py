17457bf7dc7fbbbc9f3376ea3540dc9c257ab6e3
dewmanpower <40836069+dewmanpower@users.noreply.github.com>
2019-02-07 22:22:45 +0700

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,7 @@
+"https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html"
+num = int(input("Type the number:"))
+check = int(input("Type the another number:"))
+if num%check == 0:
+    print("The number is evenly divided")
+else:
+    print("The number is not evenly divided")
